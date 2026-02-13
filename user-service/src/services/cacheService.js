98a3e1feec3543cfe6e3b7fbd59136a8f6a86038
jassisamuran <samuran3132@gmail.com>
2026-02-13 22:48:16 +0530

    cache service class

---
+++ user-service/src/services/cacheService.js
@@ -0,0 +1,47 @@
+import Redis from "io";
+import { logger } from "./src/utils/logger";
+
+export class CacheService {
+  redis;
+
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: process.env.REDIS_PORT || "6379",
+      retryStrategy: (times) => Math.min(times * 50, 2000),
+    });
+
+    this.redis.on("connect", () => {
+      logger.info("Redis Connected");
+    });
+
+    this.redis.on("error", (error) => {
+      logger.error("Redis Error");
+    });
+  }
+
+  async setUserSession(userId, data, ttl = 9000) {
+    await this.redis.setex(`user:session:${userId}`, ttl, JSON.stringify(data));
+  }
+
+  async getUserSession(userId) {
+    const data = await this.redis.get(`user:session:${userId}`);
+    return data ? JSON.parse(data) : null;
+  }
+
+  async deleteUserSession(userId) {
+    await this.redis.del(`user:session:${userId}`);
+  }
+
+  async setPasswordResetToken(email, token) {
+    await this.redis.setex(`password:reset:${email}`, 900, token);
+  }
+
+  async getPasswordResetToken(email) {
+    return await this.redis.get(`password:reset:${email}`);
+  }
+
+  async deletePasswordResetToken(email) {
+    await this.redis.del(`password:reset:${email}`);
+  }
+}
